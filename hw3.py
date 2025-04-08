f5927d0ce12cb3d794339e6385096bb7dac49560
Jordan Martinez <jrdnmrtnz12@unm.edu>
2025-04-08 00:22:01 -0600

    Added plotting from D

--- hw3.py
+++ hw3.py
@@ -21,15 +21,14 @@ import time
 from sklearn.metrics import mean_squared_error
 import idx2numpy 
 from sklearn.decomposition import PCA
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 path = ""
 
-#MNIST
+# MNIST
 test_images = idx2numpy.convert_from_file(path + "t10k-images-idx3-ubyte")
-test_images_label = idx2numpy.convert_from_file(path + "t10k-labels-idx1-ubyte").reshape(-1)
+test_images_label = idx2numpy.convert_from_file(path + "t10k-labels-idx1-ubyte")
 train_images = idx2numpy.convert_from_file(path + "train-images-idx3-ubyte")
-train_images_label = idx2numpy.convert_from_file(path + "train-labels-idx1-ubyte").reshape(-1)
+train_images_label = idx2numpy.convert_from_file(path + "train-labels-idx1-ubyte")
 
 # Flatten each image
 train_images = train_images.reshape(train_images.shape[0], -1)
@@ -40,11 +39,11 @@ scalar = StandardScaler()
 train_images_standardized = scalar.fit_transform(train_images)
 test_images_standardized = scalar.transform(test_images)
 
-#Fashion MNIST
+# Fashion MNIST
 fashion_test_images = idx2numpy.convert_from_file(path + "fashion_t10k-images-idx3-ubyte")
-fashion_test_images_label = idx2numpy.convert_from_file(path +"fashion_t10k-labels-idx1-ubyte").reshape(-1)
+fashion_test_images_label = idx2numpy.convert_from_file(path +"fashion_t10k-labels-idx1-ubyte")
 fashion_train_images = idx2numpy.convert_from_file(path + "fashion_train-images-idx3-ubyte")
-fashion_train_images_label = idx2numpy.convert_from_file(path + "fahion_train-labels-idx1-ubyte").reshape(-1)
+fashion_train_images_label = idx2numpy.convert_from_file(path + "fahion_train-labels-idx1-ubyte")
 
 # Flatten each image
 fashion_train_images = fashion_train_images.reshape(fashion_train_images.shape[0], -1)
@@ -54,31 +53,37 @@ fashion_test_images = fashion_test_images.reshape(fashion_test_images.shape[0], 
 fashion_train_images_standardized = scalar.fit_transform(fashion_train_images)
 fashion_test_images_standardized = scalar.transform(fashion_test_images)
 
-# --- Dimensionality Reduction Functions ---
-def apply_pca(X_train, X_test, n_components):
+# --- PCA Utility Functions ---
+def plot_pca_variance(pca, dataset_name, n_components):
+    eigen_vals = pca.explained_variance_
+    tot = sum(eigen_vals)
+    
+    var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+    cum_var_exp = np.cumsum(var_exp)
+
+    plt.figure(figsize=(8, 5))
+    plt.bar(range(1, n_components + 1), var_exp, align='center',
+            label='Individual explained variance')
+    plt.step(range(1, n_components + 1), cum_var_exp, where='mid',
+             label='Cumulative explained variance', color='red')
+    
+    plt.ylabel('Explained variance ratio')
+    plt.xlabel('Principal component index')
+    plt.title(f'{dataset_name} PCA Variance (n_components={n_components})')
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
+
+def apply_pca(X_train, X_test, n_components, dataset_name="Dataset"):
     pca = PCA(n_components=n_components)
     X_train_pca = pca.fit_transform(X_train)
     X_test_pca = pca.transform(X_test)
-    return X_train_pca, X_test_pca
+    var_ratio = sum(pca.explained_variance_ratio_)
+    print(f"{dataset_name} PCA (n_components={n_components}) Variance Explained: {var_ratio:.4f}")
+    plot_pca_variance(pca, dataset_name, n_components)
+    return X_train_pca, X_test_pca, var_ratio
 
-def apply_lda(X_train, X_test, y_train, n_components):
-    lda = LDA(n_components=n_components)
-    X_train_lda = lda.fit_transform(X_train, y_train)
-    X_test_lda = lda.transform(X_test)
-    return X_train_lda, X_test_lda
-
-# MNIST PCA
-mnist_pca_50_train, mnist_pca_50_test = apply_pca(train_images_standardized, test_images_standardized, 50)
-mnist_pca_100_train, mnist_pca_100_test = apply_pca(train_images_standardized, test_images_standardized, 100)
-mnist_pca_200_train, mnist_pca_200_test = apply_pca(train_images_standardized, test_images_standardized, 200)
-
-# MNIST LDA components not working for 50, 100, 200
-mnist_lda_train, mnist_lda_test = apply_lda(train_images_standardized, test_images_standardized, train_images_label, 9)
-
-#Fashion PCA
-fashion_pca_50_train, fashion_pca_50_test = apply_pca(fashion_train_images_standardized, fashion_test_images_standardized, 50)
-fashion_pca_100_train, fashion_pca_100_test = apply_pca(fashion_train_images_standardized, fashion_test_images_standardized, 100)
-fashion_pca_200_train, fashion_pca_200_test = apply_pca(fashion_train_images_standardized, fashion_test_images_standardized, 200)
-
-# Fashion LDA same issue as prev mnist lda
-fashion_lda_train, fashion_lda_test = apply_lda(fashion_train_images_standardized, fashion_test_images_standardized, fashion_train_images_label, 9)
+# --- Run PCA for Required Components ---
+for n in [50, 100, 200]:
+    mnist_pca_train, mnist_pca_test, _ = apply_pca(train_images_standardized, test_images_standardized, n, dataset_name="MNIST")
+    fashion_pca_train, fashion_pca_test, _ = apply_pca(fashion_train_images_standardized, fashion_test_images_standardized, n, dataset_name="Fashion MNIST")
